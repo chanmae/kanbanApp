26473791de2d72274f48f1eb9b5168467a0deed0
chanmae <maedayusuke2017@gmail.com>
2022-01-09 01:05:51 +0900

    Issues#2 20211125リリース_フィードバック・指摘取り込み
    指摘取り込みのため。

--- js/kanbanApp.js
+++ js/kanbanApp.js
@@ -1,73 +1,88 @@
 // タスク追加
-function addtask(){
+function addtask() {
     const todolist = document.getElementById("todolist");
 
-    const div = document.createElement("div");
-    todolist.appendChild(div);
+    // div要素生成
+    const divElement = createElement("div", todolist);
 
-    const addtask = document.createElement("input");
-    addtask.setAttribute("type","text");
-    addtask.setAttribute("size","40px");
-    div.appendChild(addtask);
+    // テキストボックス
+    const addtaskInput = document.createElement("input");
+    addtaskInput.setAttribute("type", "text");
+    addtaskInput.setAttribute("size", "40px");
+    divElement.appendChild(addtaskInput);
 
-    const addwip = document.createElement("input");
-    addwip.setAttribute("type","button");
-    addwip.setAttribute("value","WIP");
-    addwip.setAttribute("onclick","addwip(event)");
-    div.appendChild(addwip);
+    // WIPボタン
+    const wipButton = document.createElement("input");
+    wipButton.setAttribute("type", "button");
+    wipButton.setAttribute("value", "WIP");
+    wipButton.setAttribute("onclick", "addwip(event)");
+    divElement.appendChild(wipButton);
 
+    // 削除ボタン
     const adddelete = document.createElement("input");
-    adddelete.setAttribute("type","button");
-    adddelete.setAttribute("value","削除");
-    adddelete.setAttribute("onclick","deletetask(event)");
-    div.appendChild(adddelete);
+    adddelete.setAttribute("type", "button");
+    adddelete.setAttribute("value", "削除");
+    adddelete.setAttribute("onclick", "deletetask(event)");
+    divElement.appendChild(adddelete);
 }
 
 // WIP追加
-function addwip(event){
+function addwip(event) {
     // タスク削除
     deletetask(event)
     // テキストボックスのコピー
-    var todotext  = event.target.previousElementSibling;
-    // div要素作成
-    const div = document.createElement("div");
-    wiplist.appendChild(div);
+    const todotext = event.target.previousElementSibling;
+    // WIPリスト取得
+    const wiplist = document.getElementById("wiplist");
+    // div要素生成
+    const divElement = createElement("div", wiplist);
     // テキストボックス追加（TODOをコピー）
-    div.appendChild(todotext);
+    divElement.appendChild(todotext);
     // DONEボタン作成
-    const adddone = document.createElement("input");
-    adddone.setAttribute("type","button");
-    adddone.setAttribute("value","DONE");
-    adddone.setAttribute("onclick","adddone(event)");
-    div.appendChild(adddone);
+    const doneButton = document.createElement("input");
+    doneButton.setAttribute("type", "button");
+    doneButton.setAttribute("value", "DONE");
+    doneButton.setAttribute("onclick", "adddone(event)");
+    divElement.appendChild(doneButton);
     // 削除ボタン作成
-    const adddelete = document.createElement("input");
-    adddelete.setAttribute("type","button");
-    adddelete.setAttribute("value","削除");
-    adddelete.setAttribute("onclick","deletetask(event)");
-    div.appendChild(adddelete);
+    const deleteButton = document.createElement("input");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("value", "削除");
+    deleteButton.setAttribute("onclick", "deletetask(event)");
+    divElement.appendChild(deleteButton);
 }
 
 // DONE追加
-function adddone(event){
-        // タスク削除
-        deletetask(event)
-        // テキストボックスのコピー
-        var wiptext  = event.target.previousElementSibling;
-        // div要素作成
-        const div = document.createElement("div");
-        donelist.appendChild(div);
-        // テキストボックス追加（TODOをコピー）
-        div.appendChild(wiptext);
-        // 削除ボタン作成
-        const adddelete = document.createElement("input");
-        adddelete.setAttribute("type","button");
-        adddelete.setAttribute("value","削除");
-        adddelete.setAttribute("onclick","deletetask(event)");
-        div.appendChild(adddelete);    
+function adddone(event) {
+    // タスク削除
+    deletetask(event)
+    // テキストボックスのコピー（DONEボタンの前の要素取得）
+    const wiptext = event.target.previousElementSibling;
+    // DONEリスト取得
+    const donelist = document.getElementById("donelist");
+    // div要素生成
+    const divElement = createElement("div", donelist);
+    // テキストボックス追加（TODOをコピー）
+    divElement.appendChild(wiptext);
+    // 削除ボタン作成
+    const adddelete = document.createElement("input");
+    adddelete.setAttribute("type", "button");
+    adddelete.setAttribute("value", "削除");
+    adddelete.setAttribute("onclick", "deletetask(event)");
+    divElement.appendChild(adddelete);
 }
 
 // タスク削除
-function deletetask(event){
+function deletetask(event) {
     event.target.parentNode.remove();
+}
+
+// 要素生成
+function createElement(elementName, id) {
+    // 生成する要素
+    const Element = document.createElement(elementName);
+    // 指定したidの子要素に追加
+    id.appendChild(Element);
+
+    return Element;
 }
